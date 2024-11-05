a683f72c1be8e9fb08198875502803682273292c
OleksandrJTS <sashko.martynenko@gmail.com>
2024-11-05 08:14:52 +0200

    05.11.2024 menu button working outside my sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,19 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatDrawer, MatDrawerContainer, MatDrawerContent} from "@angular/material/sidenav";
 import {LeftMenuComponent} from "./components/left-menu/left-menu.component";
+import {LeftMenuService} from "./services/left-menu.service";
+import {MatToolbar} from "@angular/material/toolbar";
+import {TopToolbarComponent} from "./components/top-toolbar/top-toolbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatDrawerContainer, MatDrawer, MatDrawerContent, LeftMenuComponent],
+  imports: [RouterOutlet, MatDrawerContainer, MatDrawer, MatDrawerContent, LeftMenuComponent, TopToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'library';
+
+  constructor(public leftMenuService: LeftMenuService) { }
 }
