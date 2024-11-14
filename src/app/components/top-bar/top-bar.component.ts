0fe82d6e4f3d4950ed7847f15a02738eb689e539
OleksandrJTS <sashko.martynenko@gmail.com>
2024-11-14 07:57:33 +0200

    14.11.2024

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -5,22 +5,14 @@ import {MatIcon} from "@angular/material/icon";
 import {MatTooltip} from "@angular/material/tooltip";
 import {LeftMenuService} from "../../services/left-menu.service";
 import {NgClass} from "@angular/common";
-import {MatFormField, MatLabel, MatPrefix, MatSuffix} from "@angular/material/form-field";
+import {MatFormField, MatPrefix, MatSuffix} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
   imports: [
-    MatToolbar,
-    MatIconButton,
-    MatIcon,
-    MatTooltip,
-    NgClass,
-    MatFormField,
-    MatInput,
-    MatSuffix,
-    MatPrefix,
+    MatToolbar, MatIconButton, MatIcon, MatTooltip, NgClass, MatFormField, MatInput, MatSuffix, MatPrefix,
   ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
